618994095fb331c5a780cb00717e54e142ea72bb
nsudekenechi <nsudekenechi2@gmail.com>
2023-03-27 20:21:20 +0100

    change

--- admin/db/config.php
+++ admin/db/config.php
@@ -10,6 +10,6 @@ if ($mode=="dev") {
     define('HOST', 'localhost');
     define('USER', 'dopegztz_root');
     define('PASSWORD', 'EPfHtFtgyiAH');
-    define('DB', 'dopegztz_');
+    define('DB', 'dopegztz_DB');
 }
 $conn=mysqli_connect(HOST, USER, PASSWORD, DB);
